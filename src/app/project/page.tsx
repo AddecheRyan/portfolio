5791ecc6ad94c2dc023a46b16d207dc8cf13fa70
AddecheRyan <rnaddeche@wpi.edu>
2026-06-08 16:28:00 -0400

    removed template project posts

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -18,18 +18,6 @@ export default function ProjectPage() {
               <p className="mt-2 text-baltic/70">My senior project for my Bachelor's degree in Computer Science at Worcester Polytechnic Institute. My group and I developed a web application that increased the efficiency of the student assistant placement process.</p>
             </Link>
         </div>
-        <div className="flex flex-col gap-6 mt-8">
-            <Link key={1} href={`/project/${2}`} className="group block rounded-xl border-2 border-mint/40 bg-white p-6 shadow-sm transition-all hover:border-teal hover:shadow-md">
-              <h2 className="text-2xl font-semibold text-baltic group-hover:text-teal transition-colors">Project Title</h2>
-              <p className="mt-2 text-baltic/70">Project Content</p>
-            </Link>
-        </div>
-        <div className="flex flex-col gap-6 mt-8">
-            <Link key={1} href={`/project/${3}`} className="group block rounded-xl border-2 border-mint/40 bg-white p-6 shadow-sm transition-all hover:border-teal hover:shadow-md">
-              <h2 className="text-2xl font-semibold text-baltic group-hover:text-teal transition-colors">Project Title</h2>
-              <p className="mt-2 text-baltic/70">Project Content</p>
-            </Link>
-        </div>
       </div>
     </main>
   );
